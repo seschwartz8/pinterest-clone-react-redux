3e364407e4a99031e600b60bd9063b13e5558972
Sarah Schwartz <seschwartz8@gmail.com>
2020-04-04 09:15:56 -0700

    set up react router in app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
+import PhotoList from './photos/PhotoList';
+import PhotoCreate from './photos/PhotoCreate';
+import PhotoDetails from './photos/PhotoDetails';
+import PhotoDelete from './photos/PhotoDelete';
+import PhotoEdit from './photos/PhotoEdit';
+import Header from './Header';
+import Footer from './Footer';
+import history from '../history';
 
 function App() {
-  return <div className='App'></div>;
+  // Header/footer components are nested in router to allow use of links, but do not have a route asigned so they're always visible
+  return (
+    <div className='ui container'>
+      <Router history={history}>
+        <Header />
+        <Switch>
+          {/*Switch shows only the first route that matches*/}
+          <Route path='/' exact component={PhotoList} />
+          <Route path='/photos/new' exact component={PhotoCreate} />
+          <Route path='photo/:id' exact component={PhotoDetails} />
+          <Route path='photos/edit/:id' exact component={PhotoEdit} />
+          <Route path='/photos/delete/:id' exact component={PhotoDelete} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
