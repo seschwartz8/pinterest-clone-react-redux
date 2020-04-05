9ee5a6dd4ce80217bf44d41ae04deec0c7afc63c
Sarah Schwartz <seschwartz8@gmail.com>
2020-04-05 12:03:00 -0700

    add navbar and style to look like pinterest

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import GoogleAuth from './GoogleAuth';
+import { Link } from 'react-router-dom';
+import '../css/Header.css';
 
 const Header = () => {
   return (
-    <div>
-      <GoogleAuth />
-      <div>Header</div>
+    <div className='ui secondary pointing menu'>
+      <Link to='/' className='item'>
+        <i className='pinterest icon' />
+      </Link>
+      <div className='ui category search item'>
+        <div className='ui transparent icon input'>
+          <div className='left aligned'>
+            <i className='search link icon' />
+          </div>
+          <input
+            className='prompt'
+            type='text'
+            placeholder='Search animals...'
+          />
+        </div>
+      </div>
+
+      <Link to='/' className='item'>
+        Home
+      </Link>
+      <div className='right menu'>
+        <GoogleAuth />
+      </div>
     </div>
   );
 };
